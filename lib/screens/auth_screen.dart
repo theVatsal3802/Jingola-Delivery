6e1b24b3b7b2fd4115d3a59a057f939affffc7da
theVatsal3802 <vatsaladhiya248@gmail.com>
2022-10-26 12:52:59 +0530

    Added error handling and formatted files

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -180,6 +180,17 @@ class _AuthScreenState extends State<AuthScreen> {
                                           BottomNavigationScreen.routeName,
                                           (route) => false,
                                         );
+                                      } else {
+                                        ScaffoldMessenger.of(context)
+                                            .showSnackBar(
+                                          const SnackBar(
+                                            content: Text(
+                                              "Something went wrong, please try again.",
+                                              textScaleFactor: 1,
+                                            ),
+                                            duration: Duration(seconds: 2),
+                                          ),
+                                        );
                                       }
                                     },
                                   );
