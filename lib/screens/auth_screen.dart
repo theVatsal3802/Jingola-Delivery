f7e7f9855184fe20fa95d503d54215da2c8b409a
theVatsal3802 <vatsaladhiya248@gmail.com>
2022-10-26 12:56:54 +0530

    Added error handling and formatted files

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -165,7 +165,7 @@ class _AuthScreenState extends State<AuthScreen> {
                                     return;
                                   }
                                   _formKey.currentState!.save();
-                                  await AuthFunctions.loginChef(
+                                  await AuthFunctions.loginDeliveryPerson(
                                     context,
                                     emailController.text.trim().toLowerCase(),
                                     passwordController.text.trim(),
